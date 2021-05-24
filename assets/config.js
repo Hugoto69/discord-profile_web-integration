511536fc6dfc93189050a86adb272d4c9ae4288d
Hugo <hugoto6969@gmail.com>
2021-05-24 17:07:03 +0200

    V1.0

---
+++ assets/config.js
@@ -0,0 +1,23 @@
+const config = {
+    
+    USERNAME: "Hugoto69#4975",
+    PICTURE: "assets/img/profile_picture.png",
+
+    DESCRIPTION: "Welcome to my Discord Profile!",
+
+    BADGES: [
+        { id: "HypeSquad Balance Badge", image: "https://discord.com/assets/9fdc63ef8a3cc1617c7586286c34e4f1.svg" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/BassBoosted.png", link: "https://discord.com", name: "Bass Boosted Discord Community" },
+        { id: "project-2", image: "assets/img/BlueTechInc.png", link: "https://discord.gg/bnuC8yzFqN", name: "BlueTechnician Incorporated" },
+        { id: "project-3", image: "assets/img/ISK.png", link: "https://discord.com", name: "I.S.K. Incorporated" },
+        { id: "project-4", image: "assets/img/NexusTechInc.png", link: "https://discord.gg/9st2j6xg", name: "Nexus Technologies Incorporated"},
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/Hugoto69" },
+    ],
+
+}
